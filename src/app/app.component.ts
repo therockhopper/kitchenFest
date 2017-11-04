e8a9550cac7cff3977a8f4c999352f3c6d096261
fschmidt <frank.schmidt@redspace.com>
2017-11-04 10:01:30 -0300

    remove boilerplate code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Kitchen Fest';
+
+  videos: any[] = [
+    {title: "My Movie"},
+    {title: "Not my Movie"},
+  ]
 }
